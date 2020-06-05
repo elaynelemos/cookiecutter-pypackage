a72a64c85ee9e1fc1672497e3d48ceb76a4bc816
Elayne Lemos <elayne.l.lemos@gmail.com>
2020-06-05 15:05:48 -0300

    Update {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
    Co-authored-by: Lucas Eliaquim M. Santos <lucas_m-santos@hotmail.com>

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__main__.py
@@ -4,5 +4,5 @@ from {{cookiecutter.project_slug}}.{{cookiecutter.project_slug}} import main
 if __name__ == '__main__':
     try:
         main()
-    except Exception as e:
+    except Exception:
         print(traceback.format_exc())
